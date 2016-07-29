2d6473191571b1ba71eee7cd5f55ac3ec5f00714
TuongBui <phutuong24101990@gmail.com>
2016-07-29 15:51:54 +0700

    init

--- lib/main.rb
+++
@@ -1,8 +0,0 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
-load "callfire.rb"
-call_fire = Callfire::Client.new("c6da6a66a25ee44071ff49db722c9f8c1ebd2315")
-response = call_fire.set_callback_url("http://franchisesforsale.org/leads", true)
-puts response.to_json
-puts Callfire::VERSION
